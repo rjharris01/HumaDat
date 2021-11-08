595e1bc6261908e5a39b04d41d7e8391827d7e82
Richard Harris <Richard.j.harris@students.plymouth.ac.uk>
2021-11-08 18:16:28 +0000

    intial framework setup

---
+++ Application/server/routes/humaData-router.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const DataCtrl = require('../controllers/humaData-ctrl')
+
+const router = express.Router()
+
+router.post('/humaData', DataCtrl.addData)
+router.put('/humaData/:id', DataCtrl.updateData)
+router.delete('/humaData/:id', DataCtrl.deleteData)
+router.get('/humaData/:id', DataCtrl.getDataById)
