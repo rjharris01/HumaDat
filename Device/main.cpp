5b2cd570dd5b7f8f1f760a8dfaff19f98f628f92
rjharris01 <45001501+rjharris01@users.noreply.github.com>
2022-04-14 20:03:25 +0100

    Improved readability

--- Device/main.cpp
+++ Device/main.cpp
@@ -33,7 +33,7 @@ DHT dht(PA_1,DHTTYPE);
 ADXL345 accelerometer(PA_7, PA_6,PB_3,PB_6); //PinName mosi, PinName miso, PinName sck, PinName cs
 DigitalIn INT(PC_7);  //pin PC_7 connects to the interrupt output pin of the MAX30102
 BufferedSerial serial_port(PA_9, PA_10); 
- 
+DigitalOut led(LED1);
                  
                  
 uint32_t aun_ir_buffer[500]; //IR LED sensor data
@@ -44,69 +44,96 @@ int8_t ch_spo2_valid;   //indicator to show if the SP02 calculation is valid
 int32_t n_heart_rate;   //heart rate value
 int8_t  ch_hr_valid;    //indicator to show if the heart rate calculation is valid
 uint8_t uch_dummy;
+int accReadings[3] = {0, 0, 0};
 
 
-void checkSerial(int HR,int RL,int IR, int SP02,int VALID, int X, int Y, int Z,int HUM, int TEMP){
+//HEART BEAT//
+uint32_t un_min, un_max, un_prev_data;  //variables to calculate the on-board LED brightness that reflects the heartbeats
+uint16_t  i = 0;
+int32_t n_brightness;
+float f_temp;
+
+//Enviromental Sensor//
+uint16_t  temperature;
+uint16_t  humidity;
+uint16_t  errorEnviromentals = 0;
+
+void checkSerial(int HR,int RL,int IR, int SP02,int VALIDHR,int VALIDSP02, int X, int Y, int Z,int HUM, int TEMP){
     
-    
+    if (HR > 220){
+        HR =0;
+    }
 
     char msg[64];
 
+    if (VALIDHR == 1 && VALIDSP02 == 1){
+        snprintf(msg,64,"<%d,%d,%d,%d,%d,%d,%d,%d,%d,%d>",HR,RL,IR,SP02,VALIDHR,X,Y,Z,HUM,TEMP);
+        serial_port.write(msg, sizeof(msg));
+    }
 
+    else if(VALIDHR > 0 && VALIDSP02 == 0 ) {
+        snprintf(msg,64,"<%d,%d,%d,%d,%d,%d,%d,%d,%d,%d>",HR,RL,IR,0,1,X,Y,Z,HUM,TEMP);
+        serial_port.write(msg, sizeof(msg));
+    }
 
-    snprintf(msg,64,"<%d,%d,%d,%d,%d,%d,%d,%d,%d,%d>",HR,RL,IR,SP02,VALID,X,Y,Z,HUM,TEMP);
-    serial_port.write(msg, sizeof(msg));
+    else if(VALIDSP02 > 0 && VALIDHR == 0 ) {
+        snprintf(msg,64,"<%d,%d,%d,%d,%d,%d,%d,%d,%d,%d>",0,RL,IR,SP02,2,X,Y,Z,HUM,TEMP);
+        serial_port.write(msg, sizeof(msg));
+    }
+
+    else {
+        snprintf(msg,64,"<%d,%d,%d,%d,%d,%d,%d,%d,%d,%d>",0,RL,IR,0,3,X,Y,Z,HUM,TEMP);
+        serial_port.write(msg, sizeof(msg));
+    }
+
+    
 
 }
 
-int main()
-{
-     
 
+
+void setup(){
+    //Serial 
     serial_port.set_baud(9600);
     serial_port.set_format(
         /* bits */ 8,
         /* parity */ BufferedSerial::None,
         /* stop bit */ 1
     );
-  
-
-    
-
-    uint32_t un_min, un_max, un_prev_data;  //variables to calculate the on-board LED brightness that reflects the heartbeats
-    int i;
-    int32_t n_brightness;
-    float f_temp;
 
     //Check the sd card//
     int err = fs.mount(bd);
-
     printf("%s\r\n", (err ? "SD card Fail :(" : "SD card OK"));
-  
-   
-    if(err)
-    {
-        return 0;
-    }
-    //Check the sd card//
+
+
+    //Accelerometer
+
+    printf("Accelerometer Device ID is: 0x%02x\n", accelerometer.getDevId());
+
+    //Go into standby mode to configure the device.
+    accelerometer.setPowerControl(0x00);
+ 
+    //Full resolution, +/-16g, 4mg/LSB.
+    accelerometer.setDataFormatControl(0x0B);
+    
+    //3.2kHz data rate.
+    accelerometer.setDataRate(ADXL345_3200HZ);
+ 
+    //Measurement mode.
+    accelerometer.setPowerControl(0x08);
+
+
+    //HR
+    n_brightness=0;
+    un_min=0x3FFFF;
+    un_max=0;
+    n_ir_buffer_length=500; //buffer length of 100 stores 5 seconds of samples running at 100sps
 
     maxim_max30102_reset(); //resets the MAX30102
     ThisThread::sleep_for(1s);
     maxim_max30102_read_reg(0,&uch_dummy);
     maxim_max30102_init();  //initializes the MAX30102
-
-    n_brightness=0;
-    un_min=0x3FFFF;
-    un_max=0;
-  
-    n_ir_buffer_length=500; //buffer length of 100 stores 5 seconds of samples running at 100sps
-
-    int error = 0;
-
-    int accReadings[3] = {0, 0, 0};
-    printf("Device ID is: 0x%02x\n", accelerometer.getDevId());
-
-    //HEART BEAT//
+    
     
     //read the first 500 samples, and determine the signal range
     for(i=0;i<n_ir_buffer_length;i++)
@@ -128,44 +155,15 @@ int main()
 
     //calculate heart rate and SpO2 after first 500 samples (first 5 seconds of samples)
     maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, n_ir_buffer_length, aun_red_buffer, &n_sp02, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid); 
-    //HEART BEAT//
 
-
-
-
-     //Go into standby mode to configure the device.
-    accelerometer.setPowerControl(0x00);
- 
-    //Full resolution, +/-16g, 4mg/LSB.
-    accelerometer.setDataFormatControl(0x0B);
+    printf("setup complete\n");
     
-    //3.2kHz data rate.
-    accelerometer.setDataRate(ADXL345_3200HZ);
- 
-    //Measurement mode.
-    accelerometer.setPowerControl(0x08);
-
-    
-
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
+}
 
 
-
-    while (true) {
-      
-        //HEART BEAT//
-        i=0;
-        un_min=0x3FFFF;
-        un_max=0;
-        //HEART BEAT//
-
-
-        error = dht.readData();
-
-        
-        if (0 == error){
-             //HEART BEAT//
+void calcPPG(){
+                //HEART BEAT//
+                //dumping the first 100 sets of samples in the memory and shift the last 400 sets of samples to the top
                 for(i=100;i<500;i++)
                 {
                     aun_red_buffer[i-100]=aun_red_buffer[i];
@@ -184,64 +182,46 @@ int main()
                     un_prev_data=aun_red_buffer[i-1];
                     while(INT.read()==1);
                     maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));
-                
-                    if(aun_red_buffer[i]>un_prev_data)
-                    {
-                        f_temp=aun_red_buffer[i]-un_prev_data;
-                        f_temp/=(un_max-un_min);
-                        f_temp*=MAX_BRIGHTNESS;
-                        n_brightness-=(int)f_temp;
-                        if(n_brightness<0)
-                            n_brightness=0;
-                    }
-                    else
-                    {
-                        f_temp=un_prev_data-aun_red_buffer[i];
-                        f_temp/=(un_max-un_min);
-                        f_temp*=MAX_BRIGHTNESS;
-                        n_brightness+=(int)f_temp;
-                        if(n_brightness>MAX_BRIGHTNESS)
-                            n_brightness=MAX_BRIGHTNESS;
-                    }
-
-                    led.write(1-(float)n_brightness/256);
-                    
-                   
+        
 
                     
-
-                    
-
                 }
+                maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, n_ir_buffer_length, aun_red_buffer, &n_sp02, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid);
+                led.write(!led);
+}
 
-            //send samples and calculation result to terminal program through UART
-            printf("red=");
-            printf("%i", aun_red_buffer[i]);
-            printf(", ir=");
-            printf("%i", aun_ir_buffer[i]);
+void printDataSerial(){
+            printf("red=%i", aun_red_buffer[i]);
+            printf("ir=%i", aun_ir_buffer[i]);
             printf(", HR=%i, ", n_heart_rate); 
             printf("HRvalid=%i, ", ch_hr_valid);
             printf("SpO2=%i, ", n_sp02);
-            printf("SPO2Valid=%i\n\r", ch_spo2_valid);
-            //HEART BEAT//
-            accelerometer.getOutput(accReadings);
-            int temperature = dht.ReadTemperature(CELCIUS);
-            int humidity = dht.ReadHumidity();
+            printf("SPO2Valid=%i", ch_spo2_valid);
+            printf("%d",temperature);
+            printf("%d %% ",humidity);
+            printf("x=%i, y=%i, z=%i \n", (int16_t)accReadings[0], (int16_t)accReadings[1], (int16_t)accReadings[2]);  
+}
 
-            printf("%d\n",temperature);
-            printf("%d %% \n",humidity);
-            ThisThread::sleep_for(POLLING_RATE);
-
-            printf("%i, %i, %i\n", (int16_t)accReadings[0], (int16_t)accReadings[1], (int16_t)accReadings[2]);   
-                
-            checkSerial(n_heart_rate,aun_red_buffer[i],aun_ir_buffer[i],n_sp02,(ch_spo2_valid + ch_hr_valid),(int16_t)accReadings[0],(int16_t)accReadings[1],(int16_t)accReadings[2],humidity,temperature);
-
-         }
-         
-        else {
-            printf("Error: %d\n", error);
-            ThisThread::sleep_for(POLLING_RATE);
-         }
-         maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, n_ir_buffer_length, aun_red_buffer, &n_sp02, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid); 
+int main()
+{
+    setup();
+    
+    //HEART BEAT//
+    // Initialise the digital pin LED1 as an output
+    while (true) {
+        errorEnviromentals = dht.readData();
+        calcPPG();
+        accelerometer.getOutput(accReadings);
+        temperature = dht.ReadTemperature(CELCIUS);
+        humidity = dht.ReadHumidity();
+        // if (0 == errorEnviromentals){
+        printDataSerial();     
+        //send samples and calculation result to terminal program through UART    
+        checkSerial(n_heart_rate,aun_red_buffer[i],aun_ir_buffer[i],n_sp02,ch_hr_valid,ch_spo2_valid,(int16_t)accReadings[0],(int16_t)accReadings[1],(int16_t)accReadings[2],humidity,temperature);
+            
+        //  else {
+        //    printf("Error: with Enviromental sensor %d\n", errorEnviromentals)
+        //   }
+          ThisThread::sleep_for(POLLING_RATE);
     }
 }
