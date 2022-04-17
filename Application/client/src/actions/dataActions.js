5ce40b42ca1fb39a20321758d94267e05c6639ff
rjharris01 <45001501+rjharris01@users.noreply.github.com>
2022-04-17 02:54:33 +0100

    added delete data between dates action

--- Application/client/src/actions/dataActions.js
+++ Application/client/src/actions/dataActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {DATA_GET_DATA_ID_REQUEST,DATA_GET_DATA_ID_SUCCESS,DATA_GET_DATA_ID_FAIL,DATA_GET_INFO_FAIL,DATA_GET_INFO_SUCCESS,DATA_GET_INFO_REQUEST} from "../constants/uploadConstants"
+import {DATA_GET_DATA_ID_REQUEST,DATA_GET_DATA_ID_SUCCESS,DATA_GET_DATA_ID_FAIL,DATA_GET_INFO_FAIL,DATA_GET_INFO_SUCCESS,DATA_GET_INFO_REQUEST, DATA_DELETE_DATA_ID_REQUEST, DATA_DELETE_DATA_ID_SUCCESS, DATA_DELETE_DATA_ID_FAIL} from "../constants/dataConstants"
 
 export const getById = (dateStart,dateEnd,device_id) => async(dispatch,getState) => {
     try {
@@ -21,6 +21,32 @@ export const getById = (dateStart,dateEnd,device_id) => async(dispatch,getState)
     catch (error) {
         dispatch({
             type: DATA_GET_DATA_ID_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        })
+    }
+}
+
+
+export const deleteById = (dateStart,dateEnd,device_id) => async(dispatch,getState) => {
+    try {
+        dispatch({type: DATA_DELETE_DATA_ID_REQUEST})
+        const {userLogin:{userInfo}} = getState()
+
+        const config = {
+            headers:{
+                Authorization: `Bearer ${userInfo.token}`
+            },
+        }
+        await axios.delete(`/api/data/${dateStart}/${dateEnd}/${device_id}`,config)
+        dispatch({
+            type: DATA_DELETE_DATA_ID_SUCCESS
+        })
+       
+    } 
+    
+    catch (error) {
+        dispatch({
+            type: DATA_DELETE_DATA_ID_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message,
         })
     }
